356517f82e6ceedea2e62bcbd39b1a08db551e57
rakuda-io <rakudalms@gmail.com>
2021-11-08 07:11:27 +0900

    Create products
    firestoreに新規商品情報を登録するコンポーネントを作成。
    firestore.rulesに権限を追加して実際にfirestoreにテスト商品を登録。

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -7,13 +7,14 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
 
 //Import reducers
-// import { ProductsReducer } from '../users/reducers';
+import { ProductsReducer } from '../products/reducers';
 import { UsersReducer } from '../users/reducers';
 
 export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
       router: connectRouter(history),
+      products: ProductsReducer,
       users: UsersReducer
     }),
     applyMiddleware(
